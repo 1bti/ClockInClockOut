77086506ed156a70c4d0795064408acc667e51ef
1bti <arashid@rollins.edu>
2020-04-23 14:40:07 -0400

    4/23/20

--- multithreadedserver/ServerClientThread.java
+++ multithreadedserver/ServerClientThread.java
@@ -15,11 +15,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 class ServerClientThread extends Thread {
 	
+	
+	//Method that sends keys in script using dynamic wait times
 	public static void sendKeys(WebDriver driver, WebElement element, int timeout, String value){
 		new WebDriverWait(driver, timeout).until(ExpectedConditions.visibilityOf(element));
 		element.sendKeys(value);
 	}
 	
+	//Method that click elements in script using dynamic wait times
 	public static void clickOn(WebDriver driver, WebElement element, int timeout){
 		new WebDriverWait(driver, timeout).until(ExpectedConditions.visibilityOf(element));
 		element.click();
@@ -27,9 +30,12 @@ class ServerClientThread extends Thread {
 	
 	//clock in method needs to be in ServerClient Thread due to I/O
 	public static void clockin(String url, String username, String password, DataInputStream inStream, DataOutputStream outStream) throws InterruptedException, IOException {
+		//Driver initialization
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Log in to OKTA
 		driver.get(url);
 		WebElement user = driver.findElement(By.xpath("//*[@id='okta-signin-username']"));
 		sendKeys(driver, user, 10, username);
@@ -37,6 +43,8 @@ class ServerClientThread extends Thread {
 		sendKeys(driver,pass,10,password);
 		WebElement signIn = driver.findElement(By.xpath("//*[@id=\"okta-signin-submit\"]"));
 		clickOn(driver,signIn,100);
+		
+		//Clock in
 		WebElement clockIn = driver.findElement(By.id("ClockIn"));
 		clickOn(driver,clockIn,500);
 		Thread.sleep(2000);
@@ -45,9 +53,9 @@ class ServerClientThread extends Thread {
 		String serverMessage = "";
 		String clientMessage = "";
 		System.out.println(missedClockOut);
+		//If client has multiple jobs
 		if(!elements.isEmpty()){
-			//New Code 2/11/20
-			//List<WebElement> elements = driver.findElements(By.cssSelector("td[ng-bind='objJobCode.strDescription']"));
+			//Display Jobs
 			serverMessage="Which Job?";
 	        outStream.writeUTF(serverMessage);
 	        outStream.flush();
@@ -62,6 +70,8 @@ class ServerClientThread extends Thread {
 		        outStream.flush();
 	            i++;
 	        }
+	        
+	        //Select Jobs
 	        clientMessage=inStream.readUTF();
 	        int selection = Integer.parseInt(clientMessage);
 	        String xpathJob = "//*[@id=\'GatherJobCodeList\']/tbody/tr[" + Integer.toString(selection) + "]/td[3]";
@@ -75,12 +85,14 @@ class ServerClientThread extends Thread {
 	        outStream.flush();
 			driver.close();
 		}
+		//If client missed a clock out
 		else if(missedClockOut) {
 			serverMessage = "Missed Clock Out.";
 	        outStream.writeUTF(serverMessage);
 	        outStream.flush();
 			driver.close();
 		}
+		//If client successfully clocked in
 		else {
 			serverMessage = "Done.";
 	        outStream.writeUTF(serverMessage);
@@ -91,17 +103,22 @@ class ServerClientThread extends Thread {
 	
 	
 	public static void clockout(String url, String username, String password, DataInputStream inStream, DataOutputStream outStream) throws InterruptedException, IOException {
+		//Driver intialization
 		String serverMessage = "";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(url);
+		
+		//Log in to OKTA
 		WebElement user = driver.findElement(By.xpath("//*[@id='okta-signin-username']"));
 		sendKeys(driver, user, 10, username);
 		WebElement pass = driver.findElement(By.xpath("//*[@id=\'okta-signin-password\']"));
 		sendKeys(driver,pass,10,password);
 		WebElement signIn = driver.findElement(By.xpath("//*[@id=\"okta-signin-submit\"]"));
 		clickOn(driver,signIn,100);
+		
+		//Clock out
 		WebElement clockIn = driver.findElement(By.id("ClockOut"));
 		clickOn(driver,clockIn,500);
 		Boolean missedClockIn = driver.findElements(By.xpath("//*[@id=\'featureForm\']/div[2]/div/div/div/div[2]/div[1]")).size() > 0;
@@ -136,33 +153,31 @@ class ServerClientThread extends Thread {
 	      DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
 	      DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
 	      String clientMessage="", serverMessage="";
-	      
-	      // Socket will close when "bye" is written by client or client/server close socket
-	      while(!clientMessage.equals("bye")){  
-	        clientMessage=inStream.readUTF(); // Username input
-	        System.out.println("From Client-" +clientNo+ ": Username is :"+clientMessage);
-	        username = clientMessage;
-	        clientMessage=inStream.readUTF(); // Password input
-	        System.out.println("From Client-" +clientNo+ ": Password is :"+clientMessage);
-	        password = clientMessage;
-	        clientMessage=inStream.readUTF(); //Clock in or Clock out input
-	        serverMessage="Info recieved.";
-	        outStream.writeUTF(serverMessage);
-	        outStream.flush();
-	        if(clientMessage.equals("1")) {
-	        	System.out.println("From Client-" +clientNo+ ": Action is :Clock In");
-		        action = clientMessage;
-		        clockin(url,username,password, inStream, outStream);
-	        }
-	        else {
-	        	System.out.println("From Client-" +clientNo+ ": Action is :Clock Out");
-		        action = clientMessage;
-		        clockout(url,username, password, inStream, outStream);
-	        }
-			serverMessage="Action Completed.";
-	        outStream.writeUTF(serverMessage);
-	        outStream.flush();
+	        
+	      clientMessage=inStream.readUTF(); // Username input
+	      System.out.println("From Client-" +clientNo+ ": Username is :"+clientMessage);
+	      username = clientMessage;
+	      clientMessage=inStream.readUTF(); // Password input
+	      System.out.println("From Client-" +clientNo+ ": Password is :"+clientMessage);
+	      password = clientMessage;
+	      clientMessage=inStream.readUTF(); //Clock in or Clock out input
+	      serverMessage="Info recieved.";  //Confirmation to Client info
+	      outStream.writeUTF(serverMessage);
+	      outStream.flush();
+	      if(clientMessage.equals("1")) {
+	    	  System.out.println("From Client-" +clientNo+ ": Action is :Clock In");
+		      action = clientMessage;
+		      clockin(url,username,password, inStream, outStream);
 	      }
+	      else {
+	    	  System.out.println("From Client-" +clientNo+ ": Action is :Clock Out");
+		      action = clientMessage;
+		      clockout(url,username, password, inStream, outStream);
+	      }
+	      serverMessage="Action Completed."; //Confirmation of Action
+	      outStream.writeUTF(serverMessage);
+	      outStream.flush();
+	      
 	      inStream.close();
 	      outStream.close();
 	      serverClient.close();
--- multithreadedserver/TCPClient.java
+++ multithreadedserver/TCPClient.java
@@ -5,12 +5,15 @@ import java.io.*;
 public class TCPClient {
   public static void main(String[] args) throws Exception {
   try{
-    Socket socket=new Socket("127.0.0.1",4997);
+    //Socket socket=new Socket("127.0.0.1",4997);
+	Socket socket=new Socket("52.14.143.239",4997);
+	//I/O
     DataInputStream inStream=new DataInputStream(socket.getInputStream());
     DataOutputStream outStream=new DataOutputStream(socket.getOutputStream());
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     String clientMessage="",serverMessage="";
     
+    //Login Info & Action Request
     System.out.println("Enter username :");
     clientMessage=br.readLine();
     outStream.writeUTF(clientMessage);
@@ -23,29 +26,32 @@ public class TCPClient {
     clientMessage=br.readLine();
     outStream.writeUTF(clientMessage);
     outStream.flush();
+    
+    //Action
     serverMessage=inStream.readUTF();
     System.out.println(serverMessage);
-    if(clientMessage.equals("1")) {
+    if(clientMessage.equals("1")) { //Clock In
     	serverMessage=inStream.readUTF();
-        if(serverMessage.equals("Which Job?")) {
+        if(serverMessage.equals("Which Job?")) { //Multiple Jobs
         	System.out.println(serverMessage);
         	serverMessage=inStream.readUTF();
         	int numJobs = Integer.parseInt(serverMessage);
         	int i = 0;
-        	while(i < numJobs) {
+        	while(i < numJobs) { //Print Jobs
         		serverMessage=inStream.readUTF();
         		System.out.println(serverMessage);
         		i++;
         	}
-        	clientMessage=br.readLine();
+        	clientMessage=br.readLine(); //Choose Job
             outStream.writeUTF(clientMessage);
             outStream.flush();
         }
         serverMessage=inStream.readUTF();
         System.out.println(serverMessage);
     }
-    else {
-    	
+    else { //Clock Out
+    	serverMessage=inStream.readUTF();
+        System.out.println(serverMessage);
     }
     //outStream.close();
     //socket.close();
